7ad51e853594191bee89c6e245a9f642f2285e8e
Zully Alarcón Rodriguez <z.alarcon@uniandes.edu.co>
2022-09-13 20:05:16 -0500

    rollback

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.93.43.90"]
+ALLOWED_HOSTS = ["localhost", "ip.maquina.visualizador"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.145.75.13",  # Dirección IP de la base de datos
+        "HOST": "ip.maquina.db",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.91.152.89"
+MQTT_HOST = "ip.maquina.mqtt"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
